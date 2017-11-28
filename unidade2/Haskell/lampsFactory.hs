5aeaff4ee2e3ae08b1928f5cb329e632c2a6ed36
unknown <jfmrm@G3c07.windows.cin.ufpe.br>
2017-11-28 11:01:48 -0300

    Implements lampsFactory question in Haskell using MVar

---
+++ unidade2/Haskell/lampsFactory.hs
@@ -0,0 +1,56 @@
+module Main where
+
+import Control.Concurrent
+import Control.Concurrent.MVar
+
+makeResorce :: MVar Int -> IO()
+makeResorce resource =
+  do
+    r <- takeMVar resource
+    putMVar resource(r+1)
+    putStrLn("Feito um recurso")
+
+makeLamp :: MVar Int -> MVar Int -> MVar Int -> MVar Int -> IO()
+makeLamp sockets bulbs embalagem lamps =
+  do
+    s <- takeMVar sockets
+    b <- takeMVar bulbs
+    l <- takeMVar lamps
+    e <- takeMVar embalagem
+
+    putMVar sockets(s-1)
+    putMVar bulbs(b-1)
+    putMVar embalagem(e-1)
+    putMVar lamps(l+1)
+    putStrLn("lampada Feita")
+
+encaixota :: MVar Int -> MVar Int -> IO()
+encaixota lamps boxes =
+  do
+    l <- takeMVar lamps
+    putMVar lamps(l+1)
+
+    if l == 50 then
+      do
+        b <- takeMVar boxes
+        putMVar boxes(b+1)
+        putStrLn("caixa fechada")
+    else
+      putStrLn("lampada encaixotada")
+
+main :: IO()
+main =
+  do
+    bulbs <- newMVar 0
+    sockets <- newMVar 0
+    embalagem <- newMVar 0
+    boxes <- newMVar 0
+    lamps <- newMVar 0
+
+    forkIO(makeResorce bulbs)
+    forkIO(makeResorce sockets)
+    forkIO(makeResorce embalagem)
+    forkIO(makeLamp sockets bulbs embalagem lamps)
+    forkIO(encaixota lamps boxes)
+
+    return()
